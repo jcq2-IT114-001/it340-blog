6656923d1ccc64e62ced92165b68cf964c450d9b
jcq2-IT114-001 <jcq2@njit.edu>
2025-12-18 23:51:48 -0500

    initialized express backend with login endpoint

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://VM3_IP:27017/blogdb");
+
+const User = mongoose.model("User", {
+  username: String,
+  password: String
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username, password });
+  if (user) {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Backend running on port 3000");
+});
